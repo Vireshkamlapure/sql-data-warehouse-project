cbb47e5b617bba595691f966d46594444b1160cd
Viresh <90565006+Vireshkamlapure@users.noreply.github.com>
2026-05-02 20:49:19 +0530

    Create init_database.sql to manage DataWarehouse
    This script creates a new database named 'DataWarehouse' after checking for its existence. It drops the existing database if found, ensuring a fresh setup.

---
+++ scripts/init_database.sql
@@ -0,0 +1,21 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+-- Drop database if exists
+DROP DATABASE IF EXISTS DataWarehouse;
+
+-- Create database
+CREATE DATABASE DataWarehouse;
+
+-- Use database 
+USE DataWarehouse;
